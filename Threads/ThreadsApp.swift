231fa1d45dc13d9d12d328b660cbb7d5dcdeca7a
tsun99 <tsungaila99@gmail.com>
2023-07-10 12:30:44 +0300

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Tomas Sungaila on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
