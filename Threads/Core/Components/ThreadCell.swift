f29924f53dab3ecc6fe3db07951d5318f4cb9657
tsun99 <tsungaila99@gmail.com>
2023-07-14 22:55:07 +0300

    Basic Thread Cell View set-up

---
+++ Threads/Core/Components/ThreadCell.swift
@@ -0,0 +1,125 @@
+//
+//  ThreadCell.swift
+//  Threads
+//
+//  Created by Tomas Sungaila on 7/14/23.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Image("Harry")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60)
+                    .clipShape(Circle())
+                
+                Rectangle()
+                    .frame(width: 2)
+                    .frame(maxHeight: .infinity)
+                    .foregroundColor(.secondary)
+                
+                ZStack{
+                    
+                    Image("Harry")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20)
+                        .clipShape(Circle())
+                        .offset(x: 0, y: 30)
+                    Image("Draco")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25)
+                        .clipShape(Circle())
+                        .offset(x: -15, y: 10)
+                    Image("Ron")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30)
+                        .clipShape(Circle())
+                        .offset(x: 15, y: 5)
+                }
+            }
+            .padding(.leading)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    Text("User Name")
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Text("2h")
+                        .foregroundColor(.secondary)
+                    
+                    Button() {
+                        
+                    } label: {
+                        Image(systemName: "ellipsis")
+                    }
+                    .foregroundColor(.primary)
+                }
+                
+                
+                Text("This is the thread text. it might be long it might not be long. So if I'm writing more text ")
+                
+                Image("Harry")
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        
+                        Image(systemName: "paperplane")
+                    }
+                    Button {
+                        
+                    } label: {
+                        
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                    Button {
+                        
+                    } label: {
+                        
+                        Image(systemName: "bubble.right")
+                    }
+                    Button {
+                        
+                    } label: {
+                        
+                        Image(systemName: "heart")
+                    }
+                    
+                }
+                .foregroundColor(.primary)
+                
+                Spacer()
+                
+                HStack {
+                    Text("10 replies")
+                    Text("42 likes")
+                }
+                .foregroundColor(.secondary)
+
+            }
+            .padding(.trailing)
+            .padding(.top)
+            
+        }
+        .fixedSize(horizontal: false, vertical: true)
+        
+    }
+}
+
+struct ThreadCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ThreadCell()
+    }
+}
--- Threads/Core/Profile/Views/ProfileView.swift
+++ Threads/Core/Profile/Views/ProfileView.swift
@@ -14,98 +14,97 @@ struct ProfileView: View {
     @Namespace var animation
     
     var body: some View {
-        ScrollView {
-            // Header
-            VStack(alignment: .leading) {
-                HStack {
-                    Button {
-                        // privacy settings
-                    } label: {
-                        Image(systemName: "globe")
-                            .foregroundColor(.primary)
-                            .imageScale(.large)
-                    }
+        NavigationStack {
+            ScrollView {
+                // Header
+                VStack(alignment: .leading) {
                     
-                    Spacer()
-                    
-                    Button {
-                        // settings
-                    } label: {
-                        Image(systemName: "line.3.horizontal.decrease")
-                            .foregroundColor(.primary)
-                            .imageScale(.large)
-                    }
-                }
-                .padding(.vertical)
-            //Profile info
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Full Name")
-                            .foregroundColor(.primary)
-                            .font(.title)
-                            .bold()
-                        
-                        Text("User Name")
-                            .font(.subheadline)
-                    }
-                    Spacer()
-                    
-                    Image("Harry")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                }
-                .padding(.bottom, 10)
-                
-                
-                Text("Bio")
-                
-                Text("0 Followers")
-                    .foregroundColor(.secondary)
-                    .padding(.vertical, 5)
-                
-                Button("Edit Profile") {
-                    //Edit Profile
-                }
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .frame(height: 32)
-                .frame(maxWidth: .infinity)
-                .foregroundColor(.primary)
-                .overlay(RoundedRectangle(cornerRadius: 6)
-                    .stroke(Color.secondary, lineWidth: 1))
-                
-                HStack {
-                    ForEach(ThreadFilterViewModel.allCases, id: \.rawValue) { item in
-                        
-                        VStack {
-                            Text(item.title)
+                //Profile info
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Full Name")
+                                .foregroundColor(.primary)
+                                .font(.title)
+                                .bold()
+                            
+                            Text("User Name")
                                 .font(.subheadline)
-                                .fontWeight(selectedFilter == item ? .semibold : .regular)
-                                .foregroundColor(selectedFilter == item ? .primary : .secondary)
-                            
-                            
-                                Capsule()
-                                .foregroundColor(selectedFilter == item ? .primary : .secondary)
-                                .frame(height: 3)
-
                         }
-                        .onTapGesture {
-                            withAnimation(.easeInOut) {
-                                selectedFilter = item
+                        Spacer()
+                        
+                        Image("Harry")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                    }
+                    
+                    Text("Bio")
+                    
+                    Text("0 Followers")
+                        .foregroundColor(.secondary)
+                        .padding(.vertical, 5)
+                    
+                    Button("Edit Profile") {
+                        //Edit Profile
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .frame(height: 32)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.primary)
+                    .overlay(RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.secondary, lineWidth: 1))
+                    
+                    HStack {
+                        ForEach(ThreadFilterViewModel.allCases, id: \.rawValue) { item in
+                            
+                            VStack {
+                                Text(item.title)
+                                    .font(.subheadline)
+                                    .fontWeight(selectedFilter == item ? .semibold : .regular)
+                                    .foregroundColor(selectedFilter == item ? .primary : .secondary)
+                                
+                                
+                                    Capsule()
+                                    .foregroundColor(selectedFilter == item ? .primary : .secondary)
+                                    .frame(height: 3)
+
+                            }
+                            .onTapGesture {
+                                withAnimation(.easeInOut) {
+                                    selectedFilter = item
+                                }
                             }
                         }
                     }
+                    .overlay(Divider().offset(x: 0, y: 17))
+                    
                 }
-                .overlay(Divider().offset(x: 0, y: 17))
+                .padding()
                 
+                LazyVStack {
+                    //Threads
+                    
+                }
             }
-            .padding()
-            
-            LazyVStack {
-                //Threads
-                
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "lock")
+                    }
+                    .foregroundColor(.primary)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label : {
+                        Image(systemName: "line.3.horizontal.decrease")
+                    }
+                    .foregroundColor(.primary)
+                }
             }
         }
     }
